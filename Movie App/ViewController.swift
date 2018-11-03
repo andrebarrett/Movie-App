e5ed9976900e48c7c4c6743cf419b0752267d59e
Andre Barrett <andre@imobilize.co.uk>
2018-11-03 11:08:30 +0000

    Initial Commit

---
+++ Movie App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movie App
+//
+//  Created by Andre Barrett on 03/11/2018.
+//  Copyright © 2018 iMobilize. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
